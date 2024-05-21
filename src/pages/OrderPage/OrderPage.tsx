5dd5a7e228b87d8b9f6861f6b03295e5fe5b7513
Daniil_Ushakov <gutardanya@gmail.com>
2024-05-22 01:09:19 +0400

    create button to save filte & set Russian Language

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import styles from './OrderPage.module.css';
 import { useAppDispatch, useAppSelector } from "../../services/store";
-import { useParams } from "react-router-dom";
 import { decrement, deletePosFromOrder, increment } from "../../services/Slices/OrdersSlice";
-import Dish from "../../components/Dish/Dish";
 import { TLocalMenu, TMenu, Torder, Tposition } from "../../Utils/Types";
-import { removePosition } from "../../services/Slices/MenuSlices";
 import MenuContainer from "../../components/MenuContainer/MenuContainer";
+import { jsPDF } from 'jspdf';
+import '../../Utils/fonts/Roboto-Regular-normal'
 
 const OrderPage = () => {
 
     const dispatch = useAppDispatch()
+
+    const header = useAppSelector(state => state.OrdersSlice.currentOrder);
 
 
     const incrementPosition = async (pos: Tposition) => {
@@ -31,6 +32,13 @@ const OrderPage = () => {
         }, 0)
     }, 0)
 
+    function createAndSavePdf() {
+        var doc = new jsPDF();
+        doc.setFont("Roboto-Regular")
+        doc.text("снова попробую русский", 70, 30);
+        doc.save("a4.pdf")
+    }
+
     const removePositionFromOrder = (pos: Tposition) => {
         dispatch(deletePosFromOrder(pos))
     }
@@ -41,7 +49,7 @@ const OrderPage = () => {
                 <h1 className={styles.header}>Заказ: {order.name}</h1>
                 <div className={styles.dishesContainer}>
                     {order.dishes && order.dishes.length > 0 && order.dishes.map((menu: any) => {
-                        return <MenuContainer menu={menu} removedPos={true} incrementPos={incrementPosition} decrementPos={decrementPosition} handleClick={incrementPosition} removePosition={removePositionFromOrder}/>
+                        return <MenuContainer menu={menu} removedPos={true} incrementPos={incrementPosition} decrementPos={decrementPosition} handleClick={incrementPosition} removePosition={removePositionFromOrder} />
                     })}
                     <div className={styles.descriptions}>
                         <div className={styles.descriptionContainer}>
@@ -57,6 +65,7 @@ const OrderPage = () => {
                 <div className={styles.info}>
                     <p className={styles.totalPrice}>итоговая цена: {totalPrice}</p>
                 </div>
+                <button type="button" onClick={createAndSavePdf}>Сохранить документ</button>
             </div>
             )
             : null
